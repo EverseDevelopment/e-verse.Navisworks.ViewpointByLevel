623ee731cf6bd9a2546b8e433867913cdd9086f6
Christian Dimitri <cdimitri@e-verse.com>
2023-03-02 18:43:40 +0100

    wip

---
+++ e-verse.Navisworks.ViewpointByLevel.Installer/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using WixSharp;
+using WixSharp.Nsis;
+
+namespace EVerse.Navisworks.ViewpointByLevel.Installer
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+
+
+            var project = new ManagedProject("e-verse.Navisworks.ViewpointByLevel",
+                              new Dir(@"%AppData%\Autodesk\ApplicationPlugins\",
+                                  new Dir(@"2018",
+                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
+                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
+                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2018\bin\Release\*.*"))),
+                                  new Dir(@"2019",
+                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
+                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
+                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2019\bin\Release\*.*"))),
+                                  new Dir(@"2020",
+                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
+                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
+                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2020\bin\Release\*.*"))),
+                                  new Dir(@"2021",
+                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
+                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
+                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2021\bin\Release\*.*"))),
+                                  new Dir(@"2022",
+                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
+                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
+                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2022\bin\Release\*.*"))),
+                                  new Dir(@"2023",
+                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
+                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
+                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2023\bin\Release\*.*"))))
+                              );
+
+            project.GUID = new Guid("CA920446-C503-493F-8B3D-714340249796");
+
+            // project.ManagedUI = ManagedUI.DefaultWpf; // all stock UI dialogs
+
+            //custom set of UI WPF dialogs
+            project.ManagedUI = new ManagedUI();
+
+            project.ManagedUI.InstallDialogs.Add<EVerse.Navisworks.ViewpointByLevel.Installer.WelcomeDialog>()
+                                            .Add<EVerse.Navisworks.ViewpointByLevel.Installer.LicenceDialog>()
+                                            .Add<EVerse.Navisworks.ViewpointByLevel.Installer.ProgressDialog>()
+                                            .Add<EVerse.Navisworks.ViewpointByLevel.Installer.ExitDialog>();
+
+            project.ManagedUI.ModifyDialogs.Add<EVerse.Navisworks.ViewpointByLevel.Installer.MaintenanceTypeDialog>()
+                                           .Add<EVerse.Navisworks.ViewpointByLevel.Installer.FeaturesDialog>()
+                                           .Add<EVerse.Navisworks.ViewpointByLevel.Installer.ProgressDialog>()
+                                           .Add<EVerse.Navisworks.ViewpointByLevel.Installer.ExitDialog>();
+
+            //project.SourceBaseDir = "<input dir path>";
+            //project.OutDir = "<output dir path>";
+
+            var msiFile = project.BuildMsi();
+
+            var bootstrapper = new NsisBootstrapper
+            {
+                Primary = { FileName = msiFile },
+
+                OutputFile = "e-verse.Navisworks.ViewpointByLevel.exe",
+                IconFile = "Resources\\logo.ico",
+
+                VersionInfo = new VersionInformation("1.0.0.0")
+                {
+                    ProductName = "Test Application",
+                    LegalCopyright = "Copyright Test company",
+                    FileDescription = "Test Application",
+                    FileVersion = "1.0.0",
+                    CompanyName = "Test company",
+                    InternalName = "setup.exe",
+                    OriginalFilename = "setup.exe"
+                },
+            };
+
+            bootstrapper.Build();
+
+            //#endif
+        }
+    }
+}
