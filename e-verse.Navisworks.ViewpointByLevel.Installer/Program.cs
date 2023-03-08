3e8283be9497b9fb031ddfefa25c1f496237a029
Christian Dimitri <cdimitri@e-verse.com>
2023-03-08 16:41:36 +0100

    install .bundle folder in the app folder working

--- e-verse.Navisworks.ViewpointByLevel.Installer/Program.cs
+++ e-verse.Navisworks.ViewpointByLevel.Installer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using WixSharp;
 using WixSharp.Nsis;
 
@@ -8,33 +9,23 @@ namespace EVerse.Navisworks.ViewpointByLevel.Installer
     {
         private static void Main()
         {
-            var project = new ManagedProject("e-verse.Navisworks.ViewpointByLevel",
-                              new Dir(@"%AppData%\Autodesk\ApplicationPlugins\",
-                                  new Dir(@"2018",
-                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
-                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
-                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2018\bin\Release\*.*"))),
-                                  new Dir(@"2019",
-                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
-                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
-                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2019\bin\Release\*.*"))),
-                                  new Dir(@"2020",
-                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
-                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
-                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2020\bin\Release\*.*"))),
-                                  new Dir(@"2021",
-                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
-                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
-                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2021\bin\Release\*.*"))),
-                                  new Dir(@"2022",
-                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
-                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
-                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2022\bin\Release\*.*"))),
-                                  new Dir(@"2023",
-                                    new File(@"..\e-verse.Navisworks.ViewpointByLevel.Common\e-verse.Navisworks.ViewpointByLevel.addin"),
-                                    new Dir(@"e-verse.Navisworks.ViewpointByLevel",
-                                        new Files(@"..\e-verse.Navisworks.ViewpointByLevel.2023\bin\Release\*.*"))))
-                              );
+            // Get the directory path of the solution
+            // Get the directory path of the current executable
+            var exeDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+            // Get the solution directory path by finding the first .sln file in the parent directories
+            var solutionDirectory = Directory.GetParent(exeDirectory).Parent.Parent.FullName;
+
+            // Get the folder path by combining the solution directory path with the folder name
+            var sourceFolder = Path.Combine(solutionDirectory, "e-verse.Navisworks.ViewpointByLevel.bundle");
+
+            var files = Files.FromBuildDir(@sourceFolder, ".dll|.xml|.json|.jpg|.png|.xaml|.addin");
+            files.Attributes.Add("Component:SharedDllRefCount", "yes");
+
+            var destinationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Autodesk", "ApplicationPlugins");
+
+            // Create the project
+            var project = new ManagedProject("e-verse.Navisworks.ViewpointByLevel", new Dir(@destinationDirectory, new Dir(@"e-verse.Navisworks.ViewpointByLevel.bundle", files)));
 
             project.GUID = new Guid("CA920446-C503-493F-8B3D-714340249796");
 
